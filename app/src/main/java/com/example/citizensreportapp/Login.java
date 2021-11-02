c6a6cf4acdb0afeb12d44d883f65ff175e053261
Alexis Sanchez <53790532+jwlexi@users.noreply.github.com>
2021-11-02 02:52:14 -0700

    Starting backend work, adding internet permissions

--- app/src/main/java/com/example/citizensreportapp/Login.java
+++ app/src/main/java/com/example/citizensreportapp/Login.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 public class Login {
     private String name;
-    private String email;
 
+    private String email;
 
 }
--- app/src/main/java/com/example/citizensreportapp/MainActivity.java
+++ app/src/main/java/com/example/citizensreportapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,38 +13,105 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_main);
+
+        retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
 
         findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //handleLoginDialog();
+                handleLogin();
             }
         });
-
         findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //handleRegisterDialog();
+                handleRegister();
             }
         });
+    }
+
+    private void handleLogin() {
+        View view = getLayoutInflater().inflate(R.layout.login, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view).show();
+        Button loginBtn = view.findViewById(R.id.login);
+        EditText userEdit = view.findViewById(R.id.cin_user);
+        EditText passwordEdit = view.findViewById(R.id.cin_pass);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, String> map = new HashMap<>();
+                map.put("username", userEdit.getText().toString());
+                map.put("password", passwordEdit.getText().toString());
+                Call<Login> call = retrofitInterface.executeLogin(map);
+                call.enqueue(new Callback<Login>() {
+                    @Override
+                    public void onResponse(Call<Login> call, Response<Login> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Login> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+    }
+    private void handleRegister() {
+        View view = getLayoutInflater().inflate(R.layout.register, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view).show();
+
+        Button registerBtn = view.findViewById(R.id.register);
+        EditText userEdit = view.findViewById(R.id.cin_user);
+        EditText passwordEdit = view.findViewById(R.id.cin_pass);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, String> map = new HashMap<>();
+                map.put("username", userEdit.getText().toString());
+                map.put("password", userEdit.getText().toString());
+
+                Call<Void> call = retrofitInterface.executeRegister(map);
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
