4574a8f3d4097095fa8b4c460eecea25822a4e3c
Alexis Sanchez <53790532+jwlexi@users.noreply.github.com>
2021-11-02 13:17:59 -0700

    Updating login and register pages, removing functions

--- app/src/main/java/com/example/citizensreportapp/Home.java
+++ app/src/main/java/com/example/citizensreportapp/Home.java
@@ -18,14 +18,5 @@ public class Home extends AppCompatActivity {
 
         setContentView(R.layout.home);
 
-        Button red = findViewById(R.id.turnRed);
-        LinearLayout lol = findViewById(R.id.Var1);
-        red.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // set the color to relative layout
-                lol.setBackgroundResource(R.color.teal_700);
-            }
-        });
     }
 }
