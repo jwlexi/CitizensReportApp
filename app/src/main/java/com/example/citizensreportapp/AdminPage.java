7e29e3428ef40a23123bbb0a0ccd0b8ecf8569b1
Nayeli De Jesus <nayelidjm142@gmail.com>
2021-12-07 02:04:17 -0800

    Create admin page where user information will be displayed

---
+++ app/src/main/java/com/example/citizensreportapp/AdminPage.java
@@ -0,0 +1,14 @@
+package com.example.citizensreportapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_page);
+    }
+}
+
